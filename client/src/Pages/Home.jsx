8a5b9a0303c3a4adde973d076f203538c3fe2030
erimias46 <erimias46@gmail.com>
2023-03-24 13:40:08 +0300

    .

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,27 +2,31 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 
 import axios from 'axios'
-import { Button ,Box,Text,Flex,Spacer, Link,Card,CardHeader,Heading,CardFooter,CardBody,SimpleGrid} from '@chakra-ui/react'
+import { Button ,Box,Text,Flex,Spacer, Link,Card,CardHeader,Heading,CardFooter,CardBody,SimpleGrid,Image} from '@chakra-ui/react'
 
 const Home = () => {
     const [search, setSearch] = useState('lord')
     const [data,setData]=useState([])
     
-    const url = `https://openlibrary.org/search.json?title=${search}`;
+  const url2 = `https://api.themoviedb.org/3/search/movie?api_key=034535d00ec4b451057f7e2991109a65&language=en-US&query=${search}&page=1&include_adult=false`;
 
-    useEffect(() => {
-        handleSearch()
-    },[])
+
+  useEffect(() => {
+      console.log(process.env.REACT_APP_APIKEY)
+      handleSearch()
+      
+    },[search])
 
   const handleSearch = async (event) => {
-    
-    event.preventDefault()
-    console.log(search)
+
        
         try {
-            const response = await axios.get(url)
-          setData(response.data.docs)    
-          console.log(response)
+          const response = await axios.get(url2)
+          setData(response.data.results)
+          console.log(process.env)
+             
+          console.log(response.data.results)
+          
         }
         catch (err) {
             console.log(err)
@@ -46,7 +50,7 @@ const Home = () => {
           <Button>Login</Button>
         </Box>
       </Flex>
-      <h1>Search Books</h1>
+      <h1>Search Movies</h1>
 
       <form action="">
         <input
@@ -77,7 +81,9 @@ const Home = () => {
                 <Heading size="md"> {book.title}</Heading>
               </CardHeader>
               <CardBody>
-                <Text>here</Text>
+                <Image
+                  src={`https://image.tmdb.org/t/p/w500/${book.poster_path}`} width="100" height="100"
+                />
               </CardBody>
               <CardFooter>
                 <Button>View here</Button>
